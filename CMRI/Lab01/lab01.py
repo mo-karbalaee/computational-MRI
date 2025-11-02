66cc97adee36a78637cc5e82f984b76af57782fa
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2025-11-02 13:53:04 +0100

    improve variable names

--- CMRI/Lab01/lab01.py
+++ CMRI/Lab01/lab01.py
@@ -33,19 +33,54 @@ class Lab01_op:
         self.T2_map = mat["ph"]["t2"][0][0]  # (128, 128)
         self.PD_map = mat["ph"]["sd"][0][0]  # (128, 128)
 
+"""
+Computational Magnetic Resonance Imaging (CMRI) 2024/2025 Winter semester
+
+- Author          : Jinho Kim
+- Email           : <jinho.kim@fau.de>
+"""
+
+import numpy as np
+import utils
+
+
+class Lab01_op:
+    """
+    label: Label map of the digital brain phantom
+        - 1: Cerebrospinal fluid (CSF)
+        - 2: Gray matter (GM)
+        - 3: White matter (WM)
+    T1_map: Predefined T1 Values
+    T2_map: Predefined T2 Values
+    PD_map: Proton Density Values
+    """
+
+    def __init__(self):
+        # Define TR and TE as a pair in a list [TR, TE]
+        self.PDw_TRTE = None  # Task 2.2
+        self.T1w_TRTE = None  # Task 2.3
+        self.T2w_TRTE = None  # Task 2.4
+
+    def load_data(self, path="digital_brain_phantom.mat"):
+        mat = utils.load_data(path)
+        self.label = mat["ph"]["label"][0][0]  # (128, 128)
+        self.T1_map = mat["ph"]["t1"][0][0]  # (128, 128)
+        self.T2_map = mat["ph"]["t2"][0][0]  # (128, 128)
+        self.PD_map = mat["ph"]["sd"][0][0]  # (128, 128)
+
     def get_csf_mask(self):
         """
         Get CSF mask from the label map
         Return:
             A mask of CSF, shape: (128, 128)
         """
-        mask = np.zeros_like(self.label, dtype=np.uint8)
-        rows, cols = self.label.shape
-        for i in range(rows):
-            for j in range(cols):
-                if self.label[i, j] == 1:
-                    mask[i, j] = 1
-        return mask
+        csf_mask = np.zeros_like(self.label, dtype=np.uint8)
+        image_height, image_width = self.label.shape
+        for row_idx in range(image_height):
+            for col_idx in range(image_width):
+                if self.label[row_idx, col_idx] == 1:
+                    csf_mask[row_idx, col_idx] = 1
+        return csf_mask
 
     def get_gm_mask(self):
         """
@@ -53,13 +88,13 @@ class Lab01_op:
         Return:
             A mask of GM, shape: (128, 128)
         """
-        mask = np.zeros_like(self.label, dtype=np.uint8)
-        rows, cols = self.label.shape
-        for i in range(rows):
-            for j in range(cols):
-                if self.label[i, j] == 2:
-                    mask[i, j] = 1
-        return mask
+        gm_mask = np.zeros_like(self.label, dtype=np.uint8)
+        image_height, image_width = self.label.shape
+        for row_idx in range(image_height):
+            for col_idx in range(image_width):
+                if self.label[row_idx, col_idx] == 2:
+                    gm_mask[row_idx, col_idx] = 1
+        return gm_mask
 
     def get_wm_mask(self):
         """
@@ -67,13 +102,13 @@ class Lab01_op:
         Return:
             A mask of WM, shape: (128, 128)
         """
-        mask = np.zeros_like(self.label, dtype=np.uint8)
-        rows, cols = self.label.shape
-        for i in range(rows):
-            for j in range(cols):
-                if self.label[i, j] == 3:
-                    mask[i, j] = 1
-        return mask
+        wm_mask = np.zeros_like(self.label, dtype=np.uint8)
+        image_height, image_width = self.label.shape
+        for row_idx in range(image_height):
+            for col_idx in range(image_width):
+                if self.label[row_idx, col_idx] == 3:
+                    wm_mask[row_idx, col_idx] = 1
+        return wm_mask
 
 
     def get_T1(self, target):
@@ -85,18 +120,18 @@ class Lab01_op:
         Return:
             T1 value of the target region in [ms]
         """
-        selected_values = []
-        rows, cols = target.shape
-        for i in range(rows):
-            for j in range(cols):
-                if target[i, j] == 1:
-                    selected_values.append(self.T1_map[i, j])
+        target_region_values = []
+        image_height, image_width = target.shape
+        for row_idx in range(image_height):
+            for col_idx in range(image_width):
+                if target[row_idx, col_idx] == 1:
+                    target_region_values.append(self.T1_map[row_idx, col_idx])
 
-        selected_values = np.array(selected_values)
+        target_region_values = np.array(target_region_values)
 
-        T1_ms = np.mean(selected_values)
+        mean_T1_ms = np.mean(target_region_values)
 
-        return T1_ms
+        return mean_T1_ms
 
 
     def get_T2(self, target):
@@ -108,17 +143,17 @@ class Lab01_op:
         Return:
             T2 value of the target region in [ms]
         """
-        selected_values = []
-        rows, cols = target.shape
-        for i in range(rows):
-            for j in range(cols):
-                if target[i, j] == 1:
-                    selected_values.append(self.T2_map[i, j])
+        target_region_values = []
+        image_height, image_width = target.shape
+        for row_idx in range(image_height):
+            for col_idx in range(image_width):
+                if target[row_idx, col_idx] == 1:
+                    target_region_values.append(self.T2_map[row_idx, col_idx])
 
-        selected_values = np.array(selected_values)
-        T2_ms = np.mean(selected_values)
+        target_region_values = np.array(target_region_values)
+        mean_T2_ms = np.mean(target_region_values)
 
-        return T2_ms
+        return mean_T2_ms
 
 
     def get_PD(self, target):
@@ -130,17 +165,17 @@ class Lab01_op:
         Return:
             PD value of the target region
         """
-        selected_values = []
-        rows, cols = target.shape
-        for i in range(rows):
-            for j in range(cols):
-                if target[i, j] == 1:
-                    selected_values.append(self.PD_map[i, j])
+        target_region_values = []
+        image_height, image_width = target.shape
+        for row_idx in range(image_height):
+            for col_idx in range(image_width):
+                if target[row_idx, col_idx] == 1:
+                    target_region_values.append(self.PD_map[row_idx, col_idx])
 
-        selected_values = np.array(selected_values)
-        PD = np.mean(selected_values)
+        target_region_values = np.array(target_region_values)
+        mean_PD = np.mean(target_region_values)
 
-        return PD
+        return mean_PD
 
     def spin_echo(self, TR, TE):
         """
@@ -152,20 +187,21 @@ class Lab01_op:
         Return:
             A 2D image of the spin echo sequence, shape: (128, 128)
         """
-        SE_image = np.zeros_like(self.PD_map, dtype=float)
+        se_signal_image = np.zeros_like(self.PD_map, dtype=float)
 
-        rows, cols = self.PD_map.shape
-        for i in range(rows):
-            for j in range(cols):
-                T1 = self.T1_map[i, j]
-                T2 = self.T2_map[i, j]
-                PD = self.PD_map[i, j]
+        image_height, image_width = self.PD_map.shape
+        for row_idx in range(image_height):
+            for col_idx in range(image_width):
+                t1_value = self.T1_map[row_idx, col_idx]
+                t2_value = self.T2_map[row_idx, col_idx]
+                pd_value = self.PD_map[row_idx, col_idx]
 
-                signal = PD * (1 - np.exp(-TR / T1)) * np.exp(-TE / T2)
+                # Spin Echo Signal Equation: S = PD * (1 - exp(-TR/T1)) * exp(-TE/T2)
+                signal_intensity = pd_value * (1 - np.exp(-TR / t1_value)) * np.exp(-TE / t2_value)
 
-                SE_image[i, j] = signal
+                se_signal_image[row_idx, col_idx] = signal_intensity
 
-        return SE_image
+        return se_signal_image
 
 
 if __name__ == "__main__":
